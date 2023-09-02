fa49394ebd9939a1d8221724b607fe642113ecc9
devGiYong <akima9a@gmail.com>
2023-09-02 09:03:20 +0900

    :hammer: 인덱스 페이지 생성

--- src/main/java/com/giyong/community/controller/HomeController.java
+++ src/main/java/com/giyong/community/controller/HomeController.java
@@ -1,12 +1,23 @@
 package com.giyong.community.controller;
 
+import com.giyong.community.entity.MainCategory;
+import com.giyong.community.service.MainCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private MainCategoryService mainCategoryService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model m) {
+        Page<MainCategory> mainCategories = mainCategoryService.findAll(Pageable.unpaged());
+        m.addAttribute("mainCategories", mainCategories);
         return "index";
     }
 
